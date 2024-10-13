fef1555448680f911f0933d950232dc1f93987f4
canenbls <post@canenbley.net>
2024-10-14 01:33:48 +0200

    Add unit tests for config.py & send.py

---
+++ test/test_send.py
@@ -0,0 +1,28 @@
+from unittest.mock import MagicMock
+
+import pytest
+from pytest_mock import MockerFixture
+
+from berliner_buergerbot.send import send_message
+
+
+@pytest.fixture
+def mock_twilio(mocker: MockerFixture):
+    mock_client = MagicMock()
+    mocker.patch("berliner_buergerbot.send.Client", return_value=mock_client)
+    return mock_client
+
+
+@pytest.mark.asyncio
+async def test_send_message(mock_twilio: MagicMock):
+    mock_twilio.messages.create = MagicMock(return_value=None)
+    message = "Hello!"
+    await send_message(message)
+    mock_twilio.messages.create.assert_called_once()
+
+
+@pytest.mark.asyncio
+async def test_send_message_missing_setting(mocker: MockerFixture):
+    mocker.patch("berliner_buergerbot.send.settings.twilio_account_sid", None)
+    with pytest.raises(ValueError, match="Required Twilio settings not found."):
+        await send_message("Hello!")
