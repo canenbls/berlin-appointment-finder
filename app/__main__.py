f4d655ad9a71f42cb129cdd77d76bc6b2325f787
canenbls <post@canenbley.net>
2024-10-14 14:09:13 +0200

    Loop: Check for appointments every `settings.checking_interval` seconds

--- app/__main__.py
+++ app/__main__.py
@@ -1,4 +1,5 @@
 import asyncio
+from time import sleep
 
 from .check import check_available_appointments
 from .compose import compose_message
@@ -14,12 +15,24 @@ async def main():
             "APPOINTMENT_ID not set. Please see README.md for instructions for setting it."
         )
 
-    available_appointments = await check_available_appointments(
-        url=f"https://service.berlin.de/terminvereinbarung/termin/all/{settings.appointment_id}/",
-        playwright_headless=settings.playwright_headless,
-    )
-    message = compose_message(available_appointments)
-    await send_message(message)
+    previous_message = "No appointments available."
+
+    while True:
+        print("Checking for available appointments...")
+        available_appointments = await check_available_appointments(
+            url=f"https://service.berlin.de/terminvereinbarung/termin/all/{settings.appointment_id}/",
+            playwright_headless=settings.playwright_headless,
+        )
+        message = compose_message(available_appointments)
+
+        if message != previous_message:
+            print("News! Sending message...")
+            await send_message(message)
+        else:
+            print("No news. Not sending message.")
+
+        print(f"Sleeping for {settings.checking_interval} seconds.")
+        sleep(settings.checking_interval)
 
 
 if __name__ == "__main__":
